f99ef5ddfc232b7888403de4666b60fc15a66dce
MdAnasKhan0 <mdanaskhan.201902037@gmail.com>
2025-01-17 00:57:29 +0600

    simple form update.

--- w3PhpTutorial/day3/simpleForm.php
+++ w3PhpTutorial/day3/simpleForm.php
@@ -1,3 +1,23 @@
 <?php
-    print_r($_POST)
+    // print_r($_POST);
+    if(isset($_POST)){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $skills = implode(', ', $_POST['skills']);
+        $gender = $_POST['gender'];
+        $city = $_POST['city'];
+        echo "<br>";
+        echo "\nUser Name: ".ucfirst($name);
+        echo "<br>";
+        echo "\nUser Email: ".$email;
+        echo "<br>";
+        echo "\nPassword: ".$password;
+        echo "<br>";
+        echo "User Skills are: ".$skills;
+        echo "<br>";
+        echo "Gender: ".ucfirst($gender);
+        echo "<br>";
+        echo "City: ".ucfirst($city);
+    }
 ?>

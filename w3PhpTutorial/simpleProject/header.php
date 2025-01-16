894f542832bffd510942021d40ade006a50dec0f
MdAnasKhan0 <mdanaskhan.201902037@gmail.com>
2025-01-17 02:23:12 +0600

    simpleProject

---
+++ w3PhpTutorial/simpleProject/header.php
@@ -0,0 +1,12 @@
+<!-- Header -->
+<header>
+    <!-- logo -->
+     <h1>LOGO</h1>
+
+     <!-- navLink -->
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About us</a></li>
+        <li><a href="contact.php">Contact us</a></li>
+      </ul>
+</header>
---
+++ w3PhpTutorial/simpleProject/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link CSS -->
+     <link rel="stylesheet" href="./style.css">
+    <title>Home</title>
+</head>
+
+<body>
+
+    <!-- Header -->
+    <?php
+    include_once("./header.php");
+    ?>
+
+    <!-- Main content -->
+     <main>
+        <h1>Home Page</h1>
+     </main>
+
+    <!-- footer -->
+     <?php
+        include_once("./footer.php");
+     ?>
+
+</body>
+
+</html>
